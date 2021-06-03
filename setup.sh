5944910d744d62546139e44e6b7acda4d7bc208c
Ameise <victor.kallenbach@yahoo.de>
2021-06-03 14:44:09 -0400

    added lidar driver to setup and meta package

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 cd ..
 sudo apt-get install python3-catkin-tools
+sudo apt install build-essential cmake libglfw3-dev libglew-dev libeigen3-dev libjsoncpp-dev libtclap-dev libtins-dev libpcap-dev
 sudo rosdep init
 sudo sh -c 'echo yaml https://raw.githubusercontent.com/basler/pylon-ros-camera/master/pylon_camera/rosdep/pylon_sdk.yaml > /etc/ros/rosdep/sources.list.d/30-pylon_camera.list' && rosdep update && sudo rosdep install --from-paths ./src/drivers/submodules --ignore-src --rosdistro=noetic -y
 cd src && git submodule update --init --recursive && cd ..
